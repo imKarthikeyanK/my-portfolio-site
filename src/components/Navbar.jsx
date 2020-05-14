7fd91ba4587627387a08a2f9a0d8019d239f04f7
Karthikeyan K <superkarthi789@gmail.com>
2020-05-14 22:19:06 +0530

    adding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,19 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 
-function NavBar({pages}) {
+function NavBar({ pages }) {
     return (
         <div className="Navbar">
-        <h1>NavBar</h1>
+            <div className="brand">
+                <h1>KKK</h1>
+            </div>
+            <div className="nav">
+                {pages.map((page, index) => {
+                    return (
+                        <h3>{page.displayName}</h3>
+                    )
+                })}
+            </div>
         </div>
     )
 } export default NavBar;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,7 @@ import React, { Fragment } from 'react';
 function Home() {
     return (
         <Fragment>
-            <h1>Home</h1>
+            {/* <h1>Home</h1> */}
         </Fragment>
     )
 }export default Home;
