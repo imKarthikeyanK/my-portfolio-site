fe76361d0221dfbc5c93e7d87946f575f4e20212
Karthikeyan K <superkarthi789@gmail.com>
2020-05-15 23:21:05 +0530

    adding navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { useWindowSize } from 'react-use';
 
 
 function NavBar({ pages }) {
+    const { width } = useWindowSize()
+    console.log(";;;; window sizw", width)
     return (
         <div className="Navbar">
             <div className="brand">
                 <h1>KKK</h1>
             </div>
             <div className="nav">
-                {pages.map((page, index) => (
-                    <Link to={page.pageLink} key={index}>{page.displayName}</Link>
-                ))}
+                {width >= 768 && (
+                    pages.map((page, index) => (
+                        <Link to={page.pageLink} key={index}>{page.displayName}</Link>
+                    ))
+                )}
+                {width < 768 && (
+                    <p>X</p>
+                )}
             </div>
         </div>
     )
