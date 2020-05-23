afb60bba01b9436a52978040ee048b679c01513e
Karthikeyan K <superkarthi789@gmail.com>
2020-05-23 22:51:40 +0530

    title added

--- src/components/Home/components/LandingBlock.jsx
+++ src/components/Home/components/LandingBlock.jsx
@@ -5,6 +5,15 @@ function LandingBlock() {
     return (
         <Fragment>
             <div className="landingBlock">
+                <div className="row">
+                    <div>
+                        <p>I'm</p>
+                        <p>Karthikeyan K</p>
+                    </div>
+                    <div>
+                        <h1>Pic block</h1>
+                    </div>
+                </div>
             </div>
         </Fragment>
     )
