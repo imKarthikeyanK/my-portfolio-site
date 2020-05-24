7b03a07b0694ebae658934ea7b7bf71871513a3a
Karthikeyan K <superkarthi789@gmail.com>
2020-05-24 22:50:56 +0530

    responsive nav created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useWindowSize } from 'react-use';
+import { useWindowSize, useAsync } from 'react-use';
 import { ReactComponent as MenuIcon } from '../static/svg/menu.svg';
 // import Icon from './common/Icon';
 
 
 function NavBar({ pages }) {
-    const { width } = useWindowSize()
-    console.log(";;;; window sizw", width)
+    const { width } = useWindowSize();
+    const [expand, setExpand] = useState(false);
+    console.log(";;;; window sizw", width, expand)
     return (
         <div className="Navbar">
             <div className="brand">
@@ -20,10 +21,30 @@ function NavBar({ pages }) {
                     ))
                 )}
                 {width < 768 && (
-                    <MenuIcon />
+                    <MenuIcon onClick={() => setExpand(!expand)} />
                 )}
             </div>
+            {expand && <NavbarExtended pages={pages} expand={expand} setExpand={setExpand} />}
         </div>
     )
-} 
+}
+
+function NavbarExtended({ pages, expand, setExpand }) {
+    console.log(pages, expand)
+    return (
+        <div className="expand">
+            {pages.map((page, i) => {
+                return (
+                    <Link
+                        to={page.pageLink}
+                        key={i}
+                        onClick={()=>setExpand(false)}
+                    >
+                        {page.displayName}
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
 export default NavBar;
