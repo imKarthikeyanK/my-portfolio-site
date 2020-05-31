6de143efd204bb5c11f473db0356a880911f1056
Karthikeyan K <superkarthi789@gmail.com>
2020-06-01 00:13:35 +0530

    added careersummary component

---
+++ src/components/Home/components/CareerSummary.jsx
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+
+
+function CareerSummary(){
+    return (
+        <Fragment>
+            <h1>SUMMARY</h1>
+        </Fragment>
+    )
+} export default CareerSummary;
