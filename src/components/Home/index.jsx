62e3b2919f9911db6f172dc867736a5b56bb6f6e
Karthikeyan K <superkarthi789@gmail.com>
2020-06-05 23:44:07 +0530

    imported introduction

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
-import LandingBlock from './components/LandingBlock'
+import LandingBlock from './components/LandingBlock';
+import Introduction from './components/Introduction';
 
 
 function Home() {
     return (
         <Fragment>
             <LandingBlock />
+            <Introduction />
         </Fragment>
     )
 } export default Home;
