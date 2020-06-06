1e69cff91c77af82246fd15e6485f5480ae524a7
Karthikeyan K <superkarthi789@gmail.com>
2020-06-07 01:02:38 +0530

    footer completion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import GitImage from '../static/images/GitHub-Mark-Light-64px.png';
+import { Link } from 'react-router-dom';
 
 
 function Footer() {
@@ -6,10 +8,13 @@ function Footer() {
         <Fragment>
             <div className="footer">
                 <div className="col">
-                    git
+                    <img src={GitImage} alt="git-logo" className="git-image" />
+                    <p>Source Available <a href="https://github.com/imKKK/my-portfolio-site" rel="noopener noreferror" target="_blank">here</a></p>
                 </div>
                 <div className="col">
-                    contact
+                    <Link to={'/connect'}>
+                        <button type="button" className="connect-btn">Connect</button>
+                    </Link>
                 </div>
             </div>
         </Fragment>
