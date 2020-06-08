85826f8df1be30c879810725054482faaa9c2d7c
Karthikeyan K <superkarthi789@gmail.com>
2020-06-08 23:49:50 +0530

    card component

---
+++ src/components/common/cards.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+
+function Card() {
+    return (
+        <Fragment>
+            <div class="card">
+                <h1>Card</h1>
+            </div>
+        </Fragment>
+    )
+} export default Card;
