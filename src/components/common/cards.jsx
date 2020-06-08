d04b1ae1faf7dfd789ef1616af445c96551d337a
Karthikeyan K <superkarthi789@gmail.com>
2020-06-08 23:56:09 +0530

    added card styles

--- src/components/common/cards.jsx
+++ src/components/common/cards.jsx
@@ -4,8 +4,10 @@ import React, { Fragment } from 'react';
 function Card() {
     return (
         <Fragment>
-            <div class="card">
-                <h1>Card</h1>
+            <div class="card-container">
+                <div class="card">
+
+                </div>
             </div>
         </Fragment>
     )
