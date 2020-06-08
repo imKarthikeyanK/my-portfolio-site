0d402c81ea8ce0943596e3881db88b8068322f22
Karthikeyan K <superkarthi789@gmail.com>
2020-06-08 23:49:37 +0530

    added cards

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,10 +1,59 @@
 import React, { Fragment } from 'react';
+import Card from './common/cards';
 
+const skills = [
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    },
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    },
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    },
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    },
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    },
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    },
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    },
+    {
+        skill: "Python",
+        image: "",
+        description: ""
+    }
+]
 
 function TechStack() {
     return (
         <Fragment>
-            <h1>Tech Stack</h1>
+            <div className="skillsCard">
+                {
+                    skills.map((skill, index) => (
+                        <Card />
+                    ))
+                }
+            </div>
         </Fragment>
     )
 } export default TechStack;
