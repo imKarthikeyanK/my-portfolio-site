77141a5b50a40073d4628ad0ef107a0aa7a6258a
Karthikeyan K <superkarthi789@gmail.com>
2020-06-20 23:58:54 +0530

    support files added

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import LandingBlock from './components/LandingBlock';
 import Introduction from './components/Introduction';
+import { Helmet } from 'react-helmet';
 
 
 function Home() {
     return (
         <Fragment>
+            <Helmet>
+                <title>KKK | Home</title>
+            </Helmet>
             <LandingBlock />
             <Introduction />
         </Fragment>
