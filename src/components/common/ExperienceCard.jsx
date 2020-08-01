227f6635d50f949cd12a733dc489b49b6d27219b
Karthikeyan K <superkarthi789@gmail.com>
2020-08-01 20:02:17 +0530

    add logo field

--- src/components/common/ExperienceCard.jsx
+++ src/components/common/ExperienceCard.jsx
@@ -5,18 +5,24 @@ function ExperienceCard({ experience }) {
     return (
         <Fragment>
             <div className="experience-card">
+
                 <div className="exp-card">
-                    <div className="title">
-                        <h1>{experience.workArea}</h1>
+                    <div className="comp-logo">
+                        <img src={experience.companyLogo} alt="company-logo" height="100" width="100"></img>
                     </div>
-                    <div className="company">
-                        <h2>{experience.company}</h2>
-                    </div>
-                    <div className="work-range">
-                        <p>{experience.workRange}</p>
-                    </div>
-                    <div className="work-location">
-                        <p>{experience.workLocation}</p>
+                    <div className="content">
+                        <div className="title">
+                            <h1>{experience.workArea}</h1>
+                        </div>
+                        <div className="company">
+                            <h2>{experience.company}</h2>
+                        </div>
+                        <div className="work-range">
+                            <p>{experience.workRange}</p>
+                        </div>
+                        <div className="work-location">
+                            <p>{experience.workLocation}</p>
+                        </div>
                     </div>
                 </div>
             </div>
